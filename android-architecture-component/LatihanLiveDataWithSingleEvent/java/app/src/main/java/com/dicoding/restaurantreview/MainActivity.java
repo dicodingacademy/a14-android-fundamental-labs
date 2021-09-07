cb9aab3b9701c909537b41b4b53d97ef05fb019b
Ahmad Arif Faizin <faizin.arief@gmail.com>
2021-09-07 10:02:56 +0700

    Clean up code
    - create separate function
    - rename activityMainBinding to binding

--- android-architecture-component/LatihanLiveDataWithSingleEvent/java/app/src/main/java/com/dicoding/restaurantreview/MainActivity.java
+++ android-architecture-component/LatihanLiveDataWithSingleEvent/java/app/src/main/java/com/dicoding/restaurantreview/MainActivity.java
@@ -19,50 +19,36 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private MainViewModel mainViewModel;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        ActivityMainBinding activityMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(activityMainBinding.getRoot());
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         if (getSupportActionBar() != null) {
             getSupportActionBar().hide();
         }
 
-        mainViewModel = new ViewModelProvider(this, new ViewModelProvider.NewInstanceFactory()).get(MainViewModel.class);
+        MainViewModel mainViewModel = new ViewModelProvider(this, new ViewModelProvider.NewInstanceFactory()).get(MainViewModel.class);
 
         mainViewModel.getRestaurant().observe(this, restaurant -> {
-            activityMainBinding.tvTitle.setText(restaurant.getName());
-            activityMainBinding.tvDescription.setText(restaurant.getDescription());
-            Glide.with(this).
-                    load("https://restaurant-api.dicoding.dev/images/large/" + restaurant.getPictureId())
-                    .into(activityMainBinding.ivPicture);
+            setRestaurantData(restaurant);
         });
 
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
-        activityMainBinding.rvReview.setLayoutManager(layoutManager);
+        binding.rvReview.setLayoutManager(layoutManager);
         DividerItemDecoration itemDecoration = new DividerItemDecoration(this, layoutManager.getOrientation());
-        activityMainBinding.rvReview.addItemDecoration(itemDecoration);
+        binding.rvReview.addItemDecoration(itemDecoration);
 
         mainViewModel.getListReview().observe(this, consumerReviews -> {
-            ArrayList<String> listReview = new ArrayList<>();
-            for (CustomerReviewsItem review : consumerReviews) {
-                listReview.add(review.getReview() + "\n- " + review.getName());
-            }
-            ReviewAdapter adapter = new ReviewAdapter(listReview);
-            activityMainBinding.rvReview.setAdapter(adapter);
-            activityMainBinding.edReview.setText("");
+            setReviewData(consumerReviews);
         });
 
         mainViewModel.isLoading().observe(this, isLoading -> {
-            if (isLoading) {
-                activityMainBinding.progressBar.setVisibility(View.VISIBLE);
-            } else {
-                activityMainBinding.progressBar.setVisibility(View.GONE);
-            }
+            showLoading(isLoading);
         });
 
         mainViewModel.snackbarText().observe(this, text -> {
@@ -81,10 +67,36 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        activityMainBinding.btnSend.setOnClickListener(view -> {
-            mainViewModel.postReview(activityMainBinding.edReview.getText().toString());
+        binding.btnSend.setOnClickListener(view -> {
+            mainViewModel.postReview(binding.edReview.getText().toString());
             InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
             imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
         });
+    }
+
+    private void setRestaurantData(com.dicoding.restaurantreview.model.Restaurant restaurant) {
+        binding.tvTitle.setText(restaurant.getName());
+        binding.tvDescription.setText(restaurant.getDescription());
+        Glide.with(this).
+                load("https://restaurant-api.dicoding.dev/images/large/" + restaurant.getPictureId())
+                .into(binding.ivPicture);
+    }
+
+    private void setReviewData(java.util.List<CustomerReviewsItem> consumerReviews) {
+        ArrayList<String> listReview = new ArrayList<>();
+        for (CustomerReviewsItem review : consumerReviews) {
+            listReview.add(review.getReview() + "\n- " + review.getName());
+        }
+        ReviewAdapter adapter = new ReviewAdapter(listReview);
+        binding.rvReview.setAdapter(adapter);
+        binding.edReview.setText("");
+    }
+
+    private void showLoading(Boolean isLoading) {
+        if (isLoading) {
+            binding.progressBar.setVisibility(View.VISIBLE);
+        } else {
+            binding.progressBar.setVisibility(View.GONE);
+        }
     }
 }
