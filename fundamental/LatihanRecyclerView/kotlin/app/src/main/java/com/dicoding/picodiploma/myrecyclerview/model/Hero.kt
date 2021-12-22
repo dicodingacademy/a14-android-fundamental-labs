64b66a45267dca766da044423746b785c75613d5
Ahmad Arif Faizin <faizin.arief@gmail.com>
2021-12-22 15:24:07 +0700

    Change Parcelize

--- fundamental/LatihanRecyclerView/kotlin/app/src/main/java/com/dicoding/picodiploma/myrecyclerview/model/Hero.kt
+++ fundamental/LatihanRecyclerView/kotlin/app/src/main/java/com/dicoding/picodiploma/myrecyclerview/model/Hero.kt
@@ -1,7 +1,7 @@
 package com.dicoding.picodiploma.myrecyclerview.model
 
 import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 
 /**
  * Created by sidiqpermana on 10/29/16.
