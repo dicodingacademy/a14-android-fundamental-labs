a0910d4fe6f8b81bab093a086e75451e6ee48309
Ahmad Arif Faizin <faizin.arief@gmail.com>
2021-12-27 08:55:01 +0700

    Delete Glide in Debugging

--- fundamental/LatihanDebugging/java/app/src/main/java/com/dicoding/picodiploma/mytestingapp/MainActivity.java
+++ fundamental/LatihanDebugging/java/app/src/main/java/com/dicoding/picodiploma/mytestingapp/MainActivity.java
@@ -4,19 +4,15 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.bumptech.glide.Glide;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Button btnSetValue;
     private TextView tvText;
-    private ImageView imgPreview;
 
     private ArrayList<String> names;
 
@@ -27,7 +23,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         tvText = findViewById(R.id.tv_text);
         btnSetValue = findViewById(R.id.btn_set_value);
-        imgPreview = findViewById(R.id.img_preview);
 
         btnSetValue.setOnClickListener(this);
 
@@ -35,9 +30,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         names.add("Narenda Wicaksono");
         names.add("Kevin");
         names.add("Yoza");
-
-        //imgPreview.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.fronalpstock_big));
-        Glide.with(this).load(R.drawable.fronalpstock_big).into(imgPreview);
     }
 
     @Override
--- fundamental/LatihanDebugging/kotlin/app/src/main/java/com/dicoding/picodiploma/mytestingapp/MainActivity.kt
+++ fundamental/LatihanDebugging/kotlin/app/src/main/java/com/dicoding/picodiploma/mytestingapp/MainActivity.kt
@@ -4,17 +4,14 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import java.util.*
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var btnSetValue: Button
     private lateinit var tvText: TextView
-    private lateinit var imgPreview: ImageView
 
     private var names = ArrayList<String>()
 
@@ -24,16 +21,12 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         btnSetValue = findViewById(R.id.btn_set_value)
         tvText = findViewById(R.id.tv_text)
-        imgPreview = findViewById(R.id.img_preview)
 
         btnSetValue.setOnClickListener(this)
 
         names.add("Narenda Wicaksono")
         names.add("Kevin")
         names.add("Yoza")
-
-        imgPreview.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.fronalpstock))
-//        Glide.with(this).load(R.drawable.fronalpstock_big).into(imgPreview)
     }
 
     override fun onClick(view: View) {
