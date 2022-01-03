be23ea1fd312e4269b2b684dc4b20a7cb8985562
Ahmad Arif Faizin <faizin.arief@gmail.com>
2022-01-03 16:43:32 +0700

    add repo

--- local-data-persistent/LatihanRepository (finish)/kotlin/app/src/main/java/com/dicoding/newsapp/data/Result.kt
+++ local-data-persistent/LatihanRepository (finish)/kotlin/app/src/main/java/com/dicoding/newsapp/data/Result.kt
@@ -1,4 +1,7 @@
 package com.dicoding.newsapp.data
 
-class Result {
+sealed class Result<out R> private constructor() {
+    data class Success<out T>(val data: T) : Result<T>()
+    data class Error(val error: String) : Result<Nothing>()
+    object Loading : Result<Nothing>()
 }
