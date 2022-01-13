aad2fd189da5cc1e82405dccf52ad04c8eaa0cc0
Ahmad Arif Faizin <faizin.arief@gmail.com>
2022-01-13 15:10:54 +0700

    Rename without px

--- navigation/LatihanActionBar/kotlin/app/src/main/java/com/dicoding/picodiploma/myactionbar/MainActivity.kt
+++ navigation/LatihanActionBar/kotlin/app/src/main/java/com/dicoding/picodiploma/myactionbar/MainActivity.kt
@@ -4,7 +4,6 @@ import android.app.SearchManager
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
