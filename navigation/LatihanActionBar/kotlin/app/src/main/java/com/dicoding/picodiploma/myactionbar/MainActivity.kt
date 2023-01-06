2abb7b75b4fc4267e67ccc93a22959c1a6098a05
Ahmad Arif Faizin <faizin.arief@gmail.com>
2023-01-06 15:08:50 +0700

    Change back appcompat SearchView

--- navigation/LatihanActionBar/kotlin/app/src/main/java/com/dicoding/picodiploma/myactionbar/MainActivity.kt
+++ navigation/LatihanActionBar/kotlin/app/src/main/java/com/dicoding/picodiploma/myactionbar/MainActivity.kt
@@ -6,9 +6,9 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.SearchView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 import com.dicoding.picodiploma.myactionbar.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
