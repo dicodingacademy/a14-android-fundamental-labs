23d52e1ad12df91c1e73b3ea899a09718efbcf03
Arif <faizin.arief@gmail.com>
2023-04-10 13:34:05 +0700

    Update NoteDiffCallback

--- local-data-persistent/LatihanRoom/java/app/src/main/java/com/dicoding/picodiploma/mynoteapps/helper/NoteDiffCallback.java
+++ local-data-persistent/LatihanRoom/java/app/src/main/java/com/dicoding/picodiploma/mynoteapps/helper/NoteDiffCallback.java
@@ -9,40 +9,34 @@ import java.util.List;
 
 public class NoteDiffCallback extends DiffUtil.Callback {
 
-    private final List<Note> mOldNoteList;
-    private final List<Note> mNewNoteList;
+    private final List<Note> oldNoteList;
+    private final List<Note> newNoteList;
 
     public NoteDiffCallback(List<Note> oldNoteList, List<Note> newNoteList) {
-        this.mOldNoteList = oldNoteList;
-        this.mNewNoteList = newNoteList;
+        this.oldNoteList = oldNoteList;
+        this.newNoteList = newNoteList;
     }
 
     @Override
     public int getOldListSize() {
-        return mOldNoteList.size();
+        return oldNoteList.size();
     }
 
     @Override
     public int getNewListSize() {
-        return mNewNoteList.size();
+        return newNoteList.size();
     }
 
     @Override
     public boolean areItemsTheSame(int oldItemPosition, int newItemPosition) {
-        return mOldNoteList.get(oldItemPosition).getId() == mNewNoteList.get(newItemPosition).getId();
+        return oldNoteList.get(oldItemPosition).getId() == newNoteList.get(newItemPosition).getId();
     }
 
     @Override
     public boolean areContentsTheSame(int oldItemPosition, int newItemPosition) {
-        final Note oldEmployee = mOldNoteList.get(oldItemPosition);
-        final Note newEmployee = mNewNoteList.get(newItemPosition);
+        final Note oldNote = oldNoteList.get(oldItemPosition);
+        final Note newNote = newNoteList.get(newItemPosition);
 
-        return oldEmployee.getTitle().equals(newEmployee.getTitle()) && oldEmployee.getDescription().equals(newEmployee.getDescription());
-    }
-
-    @Nullable
-    @Override
-    public Object getChangePayload(int oldItemPosition, int newItemPosition) {
-        return super.getChangePayload(oldItemPosition, newItemPosition);
+        return oldNote.getTitle().equals(newNote.getTitle()) && oldNote.getDescription().equals(newNote.getDescription());
     }
 }
--- local-data-persistent/LatihanRoom/kotlin/app/src/main/java/com/dicoding/picodiploma/mynoteapps/helper/NoteDiffCallback.kt
+++ local-data-persistent/LatihanRoom/kotlin/app/src/main/java/com/dicoding/picodiploma/mynoteapps/helper/NoteDiffCallback.kt
@@ -3,22 +3,17 @@ package com.dicoding.picodiploma.mynoteapps.helper
 import androidx.recyclerview.widget.DiffUtil
 import com.dicoding.picodiploma.mynoteapps.database.Note
 
-class NoteDiffCallback(private val mOldNoteList: List<Note>, private val mNewNoteList: List<Note>) : DiffUtil.Callback() {
-    override fun getOldListSize(): Int {
-        return mOldNoteList.size
-    }
-
-    override fun getNewListSize(): Int {
-        return mNewNoteList.size
-    }
+class NoteDiffCallback(private val oldNoteList: List<Note>, private val newNoteList: List<Note>) : DiffUtil.Callback() {
+    override fun getOldListSize(): Int = oldNoteList.size
+    override fun getNewListSize(): Int = newNoteList.size
 
     override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
-        return mOldNoteList[oldItemPosition].id == mNewNoteList[newItemPosition].id
+        return oldNoteList[oldItemPosition].id == newNoteList[newItemPosition].id
     }
 
     override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
-        val oldEmployee = mOldNoteList[oldItemPosition]
-        val newEmployee = mNewNoteList[newItemPosition]
-        return oldEmployee.title == newEmployee.title && oldEmployee.description == newEmployee.description
+        val oldNote = oldNoteList[oldItemPosition]
+        val newNote = newNoteList[newItemPosition]
+        return oldNote.title == newNote.title && oldNote.description == newNote.description
     }
 }
