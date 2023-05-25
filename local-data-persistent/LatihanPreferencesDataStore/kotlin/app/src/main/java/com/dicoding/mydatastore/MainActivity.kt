9e4dfedbb49146b066d6eb4a5ada5cedc6e921c7
Arif <faizin.arief@gmail.com>
2023-05-25 21:47:07 +0700

    move datastore init

--- local-data-persistent/LatihanPreferencesDataStore/kotlin/app/src/main/java/com/dicoding/mydatastore/MainActivity.kt
+++ local-data-persistent/LatihanPreferencesDataStore/kotlin/app/src/main/java/com/dicoding/mydatastore/MainActivity.kt
@@ -1,17 +1,12 @@
 package com.dicoding.mydatastore
 
-import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.CompoundButton
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
-import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.preferencesDataStore
 import androidx.lifecycle.ViewModelProvider
 import com.google.android.material.switchmaterial.SwitchMaterial
 
-private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,7 +16,7 @@ class MainActivity : AppCompatActivity() {
 
         val switchTheme = findViewById<SwitchMaterial>(R.id.switch_theme)
 
-        val pref = SettingPreferences.getInstance(dataStore)
+        val pref = SettingPreferences.getInstance(application.dataStore)
         val mainViewModel = ViewModelProvider(this, ViewModelFactory(pref)).get(
             MainViewModel::class.java
         )
--- local-data-persistent/LatihanPreferencesDataStore/kotlin/app/src/main/java/com/dicoding/mydatastore/SettingPreferences.kt
+++ local-data-persistent/LatihanPreferencesDataStore/kotlin/app/src/main/java/com/dicoding/mydatastore/SettingPreferences.kt
@@ -1,11 +1,15 @@
 package com.dicoding.mydatastore
 
+import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.preferencesDataStore
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
+
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
 
 class SettingPreferences private constructor(private val dataStore: DataStore<Preferences>) {
 
