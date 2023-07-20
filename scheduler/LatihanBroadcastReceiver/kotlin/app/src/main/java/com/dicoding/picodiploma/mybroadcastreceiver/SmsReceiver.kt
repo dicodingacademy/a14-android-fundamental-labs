0a4a26e9bc494535e2fa0a51d8624dfe9aa98f78
Arif <faizin.arief@gmail.com>
2023-07-20 11:14:26 +0700

    Change SmsMessage.createFromPdu to Telephony.Sms.Intents

--- scheduler/LatihanBroadcastReceiver/kotlin/app/src/main/java/com/dicoding/picodiploma/mybroadcastreceiver/SmsReceiver.kt
+++ scheduler/LatihanBroadcastReceiver/kotlin/app/src/main/java/com/dicoding/picodiploma/mybroadcastreceiver/SmsReceiver.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import android.provider.Telephony
 import android.telephony.SmsMessage
 import android.util.Log
 
@@ -15,36 +16,18 @@ class SmsReceiver : BroadcastReceiver() {
     }
 
     override fun onReceive(context: Context, intent: Intent) {
-        val bundle = intent.extras
-        try {
-            if (bundle != null) {
-                /*
-                Bundle dengan key "pdus" sudah merupakan standar yang digunakan oleh system
-                 */
-                val pdusObj = bundle.get("pdus") as Array<*>
-                for (aPdusObj in pdusObj) {
-                    val currentMessage = getIncomingMessage(aPdusObj as Any, bundle)
-                    val senderNum = currentMessage.displayOriginatingAddress
-                    val message = currentMessage.displayMessageBody
-                    Log.d(TAG, "senderNum: $senderNum; message: $message")
-                    val showSmsIntent = Intent(context, SmsReceiverActivity::class.java)
-                    showSmsIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
-                    showSmsIntent.putExtra(SmsReceiverActivity.EXTRA_SMS_NO, senderNum)
-                    showSmsIntent.putExtra(SmsReceiverActivity.EXTRA_SMS_MESSAGE, message)
-                    context.startActivity(showSmsIntent)
-                }
+        if (intent.action == Telephony.Sms.Intents.SMS_RECEIVED_ACTION) {
+            val messages = Telephony.Sms.Intents.getMessagesFromIntent(intent)
+            for (message in messages) {
+                val senderNum = message.originatingAddress
+                val body = message.messageBody
+                Log.d(TAG, "senderNum: $senderNum; message: $message")
+                val showSmsIntent = Intent(context, SmsReceiverActivity::class.java)
+                showSmsIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                showSmsIntent.putExtra(SmsReceiverActivity.EXTRA_SMS_NO, senderNum)
+                showSmsIntent.putExtra(SmsReceiverActivity.EXTRA_SMS_MESSAGE, body)
+                context.startActivity(showSmsIntent)
             }
-        } catch (e: Exception) {
-            Log.d(TAG, "Exception smsReceiver $e")
         }
-    }
-
-    private fun getIncomingMessage(aObject: Any, bundle: Bundle): SmsMessage {
-        val currentSMS: SmsMessage
-        val format = bundle.getString("format")
-        currentSMS = if (Build.VERSION.SDK_INT >= 23) {
-            SmsMessage.createFromPdu(aObject as ByteArray, format)
-        } else SmsMessage.createFromPdu(aObject as ByteArray)
-        return currentSMS
     }
 }
