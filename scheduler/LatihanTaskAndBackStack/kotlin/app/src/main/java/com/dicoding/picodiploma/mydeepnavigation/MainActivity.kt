4a51ce001ea59ec2a2e01bd9c64389db93e822ce
Arif <faizin.arief@gmail.com>
2023-07-21 14:19:52 +0700

    adjust with LatihanNotification

--- scheduler/LatihanTaskAndBackStack/kotlin/app/src/main/java/com/dicoding/picodiploma/mydeepnavigation/MainActivity.kt
+++ scheduler/LatihanTaskAndBackStack/kotlin/app/src/main/java/com/dicoding/picodiploma/mydeepnavigation/MainActivity.kt
@@ -6,7 +6,6 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import android.media.RingtoneManager
 import android.os.Build
 import android.os.Bundle
 import android.view.View
@@ -15,7 +14,6 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
 import androidx.core.app.TaskStackBuilder
-import androidx.core.content.ContextCompat
 import com.dicoding.picodiploma.mydeepnavigation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
@@ -42,21 +40,23 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
         }
 
+        binding.btnSendNotification.setOnClickListener(this)
         binding.btnOpenDetail.setOnClickListener(this)
-
-        showNotification(this@MainActivity, getString(R.string.notification_title), getString(R.string.notification_message), 110)
-
     }
 
 
     override fun onClick(v: View) {
-        if (v.id == R.id.btn_open_detail) {
+        val title = getString(R.string.notification_title)
+        val message = getString(R.string.notification_message)
+        if (v.id == R.id.btn_send_notification) {
+            sendNotification(title, message)
+        } else if (v.id == R.id.btn_open_detail) {
             /*
             Intent yang akan dikirimkan ke halaman detail
              */
             val detailIntent = Intent(this@MainActivity, DetailActivity::class.java)
-            detailIntent.putExtra(DetailActivity.EXTRA_TITLE, getString(R.string.detail_title))
-            detailIntent.putExtra(DetailActivity.EXTRA_MESSAGE, getString(R.string.detail_message))
+            detailIntent.putExtra(DetailActivity.EXTRA_TITLE, title)
+            detailIntent.putExtra(DetailActivity.EXTRA_MESSAGE, message)
             startActivity(detailIntent)
         }
     }
@@ -69,61 +69,47 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
      * @param message pesan notifikasi
      * @param notifId id dari notifikasi
      */
-    private fun showNotification(context: Context, title: String, message: String, notifId: Int) {
-        val CHANNEL_ID = "Channel_1"
-        val CHANNEL_NAME = "Navigation channel"
+    private fun sendNotification(title: String, message: String) {
 
         val notifDetailIntent = Intent(this, DetailActivity::class.java)
-        /*
-        Intent yang akan dikirimkan ke halaman detail
-        */
         notifDetailIntent.putExtra(DetailActivity.EXTRA_TITLE, title)
         notifDetailIntent.putExtra(DetailActivity.EXTRA_MESSAGE, message)
 
-        val pendingIntent = TaskStackBuilder.create(this).run{
-            addParentStack(DetailActivity::class.java)
-            addNextIntent(notifDetailIntent)
+        val pendingIntent = TaskStackBuilder.create(this).run {
+            addNextIntentWithParentStack(notifDetailIntent)
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                getPendingIntent(110, PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE)
+                getPendingIntent(NOTIFICATION_ID, PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE)
             } else {
-                getPendingIntent(110, PendingIntent.FLAG_UPDATE_CURRENT)
+                getPendingIntent(NOTIFICATION_ID, PendingIntent.FLAG_UPDATE_CURRENT)
             }
         }
 
-        val notificationManagerCompat = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        val alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
-        val builder = NotificationCompat.Builder(context, CHANNEL_ID)
-                .setContentTitle(title)
-                .setSmallIcon(R.drawable.ic_email_black_24dp)
-                .setContentText(message)
-                .setColor(ContextCompat.getColor(context, android.R.color.black))
-                .setVibrate(longArrayOf(1000, 1000, 1000, 1000, 1000))
-                .setSound(alarmSound)
-                .setContentIntent(pendingIntent)
-                .setAutoCancel(true)
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentTitle(title)
+            .setSmallIcon(R.drawable.baseline_notifications_active_24)
+            .setContentText(message)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setSubText(getString(R.string.notification_subtext))
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
 
-        /*
-        Untuk android Oreo ke atas perlu menambahkan notification channel
-        Materi ini akan dibahas lebih lanjut di modul extended
-         */
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-
-
-            /* Create or update. */
-            val channel = NotificationChannel(CHANNEL_ID,
-                    CHANNEL_NAME,
-                    NotificationManager.IMPORTANCE_DEFAULT)
-
-            channel.enableVibration(true)
-            channel.vibrationPattern = longArrayOf(1000, 1000, 1000, 1000, 1000)
-
+            val channel = NotificationChannel(
+                CHANNEL_ID,
+                CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
             builder.setChannelId(CHANNEL_ID)
-
-            notificationManagerCompat.createNotificationChannel(channel)
+            notificationManager.createNotificationChannel(channel)
         }
-
         val notification = builder.build()
+        notificationManager.notify(NOTIFICATION_ID, notification)
+    }
 
-        notificationManagerCompat.notify(notifId, notification)
+    companion object {
+        private const val NOTIFICATION_ID = 1
+        private const val CHANNEL_ID = "channel_01"
+        private const val CHANNEL_NAME = "dicoding channel"
     }
 }
