152efc3e4a23db564402ac4661f1b328066090a1
Arif <faizin.arief@gmail.com>
2023-07-21 15:14:46 +0700

    adjust with Latihan StackAndBackStack

--- scheduler/LatihanNotification/kotlin/app/src/main/java/com/dicoding/picodiploma/simplenotif/MainActivity.kt
+++ scheduler/LatihanNotification/kotlin/app/src/main/java/com/dicoding/picodiploma/simplenotif/MainActivity.kt
@@ -15,8 +15,11 @@ import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
+import com.dicoding.picodiploma.simplenotif.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
 
     private val requestPermissionLauncher =
         registerForActivityResult(
@@ -31,7 +34,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         /*
         Untuk Android 13 ke atas perlu menambahkan permission
@@ -39,10 +43,16 @@ class MainActivity : AppCompatActivity() {
         if (Build.VERSION.SDK_INT >= 33) {
             requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
         }
+
+        val title = getString(R.string.notification_title)
+        val message = getString(R.string.notification_message)
+
+        binding.btnSendNotification.setOnClickListener {
+            sendNotification(title, message)
+        }
     }
 
-    //aksi untuk onClick pada button
-    fun sendNotification(view: View) {
+    private fun sendNotification(title: String, message: String) {
         val intent = Intent(Intent.ACTION_VIEW, Uri.parse("http://dicoding.com"))
         val pendingIntent = PendingIntent.getActivity(
             this,
@@ -53,30 +63,24 @@ class MainActivity : AppCompatActivity() {
 
         val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         val builder = NotificationCompat.Builder(this, CHANNEL_ID)
-                .setContentIntent(pendingIntent)
-                .setSmallIcon(R.drawable.ic_notifications_white_48px)
-                .setLargeIcon(BitmapFactory.decodeResource(resources, R.drawable.ic_notifications_white_48px))
-                .setContentTitle(resources.getString(R.string.content_title))
-                .setContentText(resources.getString(R.string.content_text))
-                .setSubText(resources.getString(R.string.subtext))
-                .setAutoCancel(true)
-
-        /*
-        Untuk Android Oreo ke atas perlu menambahkan notification channel
-        */
+            .setContentTitle(title)
+            .setSmallIcon(R.drawable.baseline_notifications_active_24)
+            .setContentText(message)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setSubText(getString(R.string.notification_subtext))
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-
-            /* Create or update. */
-            val channel = NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT)
-            channel.description = CHANNEL_NAME
-
+            val channel = NotificationChannel(
+                CHANNEL_ID,
+                CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
             builder.setChannelId(CHANNEL_ID)
             notificationManager.createNotificationChannel(channel)
         }
-
         val notification = builder.build()
-
         notificationManager.notify(NOTIFICATION_ID, notification)
     }
 
