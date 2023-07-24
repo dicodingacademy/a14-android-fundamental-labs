425bfff6eb85aa68bf8d8a171b14a9e61345560b
Arif <faizin.arief@gmail.com>
2023-07-24 09:54:09 +0700

    Add notification permission

--- scheduler/LatihanAlarmManager/kotlin/app/src/main/java/com/dicoding/picodiploma/myalarmmanager/MainActivity.kt
+++ scheduler/LatihanAlarmManager/kotlin/app/src/main/java/com/dicoding/picodiploma/myalarmmanager/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.dicoding.picodiploma.myalarmmanager
 
+import android.Manifest
+import android.os.Build
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.dicoding.picodiploma.myalarmmanager.databinding.ActivityMainBinding
 import com.dicoding.picodiploma.myalarmmanager.utils.DatePickerFragment
@@ -20,11 +24,25 @@ class MainActivity : AppCompatActivity(), View.OnClickListener, DatePickerFragme
         private const val TIME_PICKER_REPEAT_TAG = "TimePickerRepeat"
     }
 
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                Toast.makeText(this, "Notifications permission granted", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Notifications permission rejected", Toast.LENGTH_SHORT).show()
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding?.root)
+
+        if (Build.VERSION.SDK_INT >= 33) {
+            requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+        }
 
         // Listener one time alarm
         binding?.btnOnceDate?.setOnClickListener(this)
