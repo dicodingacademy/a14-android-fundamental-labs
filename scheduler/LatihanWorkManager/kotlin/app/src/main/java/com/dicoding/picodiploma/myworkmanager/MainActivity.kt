6368016ed030da6e256e3874912dd45e25d8ead8
Arif <faizin.arief@gmail.com>
2023-07-25 09:31:53 +0700

    Add notification permission

--- scheduler/LatihanWorkManager/kotlin/app/src/main/java/com/dicoding/picodiploma/myworkmanager/MainActivity.kt
+++ scheduler/LatihanWorkManager/kotlin/app/src/main/java/com/dicoding/picodiploma/myworkmanager/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.dicoding.picodiploma.myworkmanager
 
+import android.Manifest
+import android.os.Build
 import android.os.Bundle
 import android.view.View
 import android.view.View.OnClickListener
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.work.*
 import androidx.work.Data.Builder
@@ -15,10 +19,25 @@ class MainActivity : AppCompatActivity(), OnClickListener {
     private lateinit var periodicWorkRequest: PeriodicWorkRequest
     private lateinit var binding: ActivityMainBinding
 
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                Toast.makeText(this, "Notifications permission granted", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Notifications permission rejected", Toast.LENGTH_SHORT).show()
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        if (Build.VERSION.SDK_INT >= 33) {
+            requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+        }
 
         workManager = WorkManager.getInstance(this)
 
