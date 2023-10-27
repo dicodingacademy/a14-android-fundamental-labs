4e1bf109e2fccc6a8f9db01e734baf69d3596ced
Ahmad Arif Faizin <faizin.arief@gmail.com>
2023-10-27 21:00:07 +0700

    Update AGP 8.1 & targetSdk 34

--- local-data-persistent/LatihanSharedPreference/kotlin/app/build.gradle.kts
+++ local-data-persistent/LatihanSharedPreference/kotlin/app/build.gradle.kts
@@ -6,11 +6,11 @@ plugins {
 
 android {
     namespace = "com.dicoding.picodiploma.mysharedpreference"
-    compileSdk = 33
+    compileSdk = 34
     defaultConfig {
         applicationId = "com.dicoding.picodiploma.mysharedpreference"
         minSdk = 21
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
@@ -42,7 +42,4 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-}
-repositories {
-    mavenCentral()
 }
