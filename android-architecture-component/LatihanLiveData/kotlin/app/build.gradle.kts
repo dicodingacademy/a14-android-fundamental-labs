a7542ecaaa5d7aaa385c7e6b71dccae41a4cb2c5
IanDs <ian.indratama@gmail.com>
2023-11-06 13:40:50 +0700

    Latihan LiveData - Update Dependencies & Material3, delete Java code in project

--- android-architecture-component/LatihanLiveData/kotlin/app/build.gradle.kts
+++ android-architecture-component/LatihanLiveData/kotlin/app/build.gradle.kts
@@ -41,7 +41,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("com.google.android.material:material:1.10.0")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test:runner:1.5.2")
