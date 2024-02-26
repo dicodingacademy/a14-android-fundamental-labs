6e97e43de593c10445007e95e4b21f86d4494535
ianindratama <ian.indratama@gmail.com>
2024-02-26 15:35:48 +0700

    Updated - Latihan Tab Layout dengan ViewPager2

--- navigation/LatihanTabLayout/kotlin/app/build.gradle.kts
+++ navigation/LatihanTabLayout/kotlin/app/build.gradle.kts
@@ -30,14 +30,12 @@ android {
 }
 
 dependencies {
-    implementation("androidx.appcompat:appcompat:1.4.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.2")
-    implementation(kotlin("stdlib"))
-    implementation("androidx.legacy:legacy-support-v4:1.0.0")
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.3")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
-    implementation("com.google.android.material:material:1.4.0")
-    implementation("androidx.core:core-ktx:1.7.0")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("androidx.viewpager2:viewpager2:1.0.0")
 }
